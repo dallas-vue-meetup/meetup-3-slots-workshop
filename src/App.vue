b3c10469e6e924eb6bce208efb75c723f370f033
Tim Waite <tim@dialexa.com>
2019-04-14 15:01:40 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <d-header/>
+    <main class="app-content">
+      <h1 >Welcome to the workshop!</h1>
+    </main>
+  </div>
+</template>
+
+<script>
+import DHeader from '@/components/common/DHeader.vue';
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'app',
+  components: {
+    DHeader,
+  },
+  data() {
+    return {
+      loading: true,
+    }
+  },
+  methods: {
+    ...mapActions([
+      'loadData',
+    ]),
+  },
+  created() {
+    this.loadData().then(() => {
+      this.loading = false;
+    });
+  }
+}
+</script>
+
+<style lang="postcss">
+@tailwind preflight;
+@tailwind components;
+@tailwind utilities;
+
+* {
+  font-family: 'Noto Sans JP', sans-serif;
+}
+
+#app {
+  @apply bg-grey-lighter min-h-screen flex flex-col;
+}
+
+.app-content {
+  @apply flex flex-col flex-grow justify-center items-center mx-4;
+}
+</style>
